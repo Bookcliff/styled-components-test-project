d3af850878f5e7ac8bce8d5e5fd295144c52e72a
Bookcliff <bookcliff76@gmail.com>
2022-10-21 12:29:06 -0500

    footer styles

---
+++ src/components/Socialicons.js
@@ -0,0 +1,20 @@
+import { StyledSocialIcons } from "./styles/SocialIcons.styled";
+import { FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+export default function SocialIcons() {
+  return (
+    <StyledSocialIcons>
+      <li>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://linkedin.com">
+          <FaLinkedin />
+        </a>
+      </li>
+    </StyledSocialIcons>
+  );
+}
